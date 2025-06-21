90613c3915d72f813b7096f3c9e439fa38f27f9a
Jatin Banga <jatin.b.rx3@gmail.com>
2025-06-21 19:39:47 +0530

    small change to build in pyproject.toml

--- src/matrx_utils/__init__.py
+++ src/matrx_utils/__init__.py
@@ -2,6 +2,7 @@ from .fancy_prints import vclist, vcprint, pretty_print, print_link, print_trunc
 from .fancy_prints.matrx_print_logger import MatrixPrintLog
 from .data_handling import DataTransformer
 from .data_handling.validation.validators import URLValidator, validate_url, validate_email
-from .utils import generate_directory_structure, generate_and_save_directory_structure, clear_terminal
+from .utils import generate_directory_structure, generate_and_save_directory_structure, clear_terminal 
+from .file_handling import FileManager
 
-__all__ = ["vclist", "vcprint", "pretty_print", "print_link", "print_truncated", "MatrixPrintLog", "DataTransformer", "URLValidator", "validate_url", "validate_email", "generate_directory_structure", "generate_and_save_directory_structure", "clear_terminal"]
+__all__ = ["vclist", "vcprint", "pretty_print", "print_link", "print_truncated", "MatrixPrintLog", "DataTransformer", "URLValidator", "validate_url", "validate_email", "generate_directory_structure", "generate_and_save_directory_structure", "clear_terminal", "FileManager"]
