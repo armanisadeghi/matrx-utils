6446e96919f394228985fa3fa56b2fbc89d24726
Jatin Banga <jatin.b.rx3@gmail.com>
2025-06-21 19:07:03 +0530

    added file handling

---
+++ src/matrx_utils/file_handling/__init__.py
@@ -0,0 +1,11 @@
+from .file_manager import FileManager
+from .file_handler import FileHandler
+from .batch_handler import BatchHandler
+from .base_handler import BaseHandler
+
+__all__ = [
+    'FileManager',
+    'FileHandler',
+    'BatchHandler',
+    'BaseHandler'
+]
